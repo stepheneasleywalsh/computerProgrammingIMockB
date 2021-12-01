4a39a80f3ef6f3781f937c6b0688ca8d1ecdf6e5
stephen.easley-walsh <stephen.easley-walsh@studygroup.com>
2021-12-01 09:49:49 +0000

    Update

--- main.py
+++ main.py
@@ -1,64 +1,55 @@
-# Import Math for GCD
+# Import Math, needed for GCD calculation
 import math
 
-# Keep running unless breaks out
 while True:
-    # Input loop
-    validInput = False
-    while validInput == False:
+    while True:
+        # Ask for A and B
+        print(" # # # Tell me where your line crosses the X and Y axis. Values must be integers. # # #")
         try:
-            print(" # # # Tell me where your line crosses the X and Y axis. Values must be integers. # # #")
-            x = int(input("Where does your line cross the X-axis? "))
-            y = int(input("Where does your line cross the Y-axis? "))
-            if x == 0 and y == 0:
-                print("Sorry, I need two points.")
-            elif x == 0 and not y == 0:
+            A = int(input("Where does your line cross the X-axis? "))
+            B = int(input("Where does your line cross the Y-axis? "))
+            # Check that they are not zero
+            if A == 0 and not B == 0:
                 print("That is just the Y-axis i.e. x=0")
                 print("Give me non-zero integers please :)")
-            elif not x == 0 and y == 0:
+            elif not A == 0 and B == 0:
                 print("That is just the X-axis i.e. y=0")
                 print("Give me non-zero integers please :)")
+            elif A == 0 and B == 0:
+                print("Sorry, I need two points.")
             else:
-                print("Thank you. I will find the equation of the line.")
-                validInput = True
+                break
         except:
             print("Sorry, that is not a number")
 
-    # Find the coefficients
-    A = x
-    B = y
-    C = x*y
-
-    # Divide out the GCD
-    GCD = math.gcd(A,B,C)
-    A /= GCD
+    # Calculating the equation of the line
+    C = A*B
+    GCD = math.gcd(B,A,C)
     B /= GCD
+    A /= GCD
     C /= GCD
-
-    # Lead with positive only
-    if B < 0:
-        A *= -1
-        B *= -1
-        C *= -1
-
-    # Force integers
-    A = int(A)
     B = int(B)
+    A = int(A)
     C = int(C)
 
-    # Clean the ouput and print it
+    # Making sure it doesn't start with minus
+    if B < 0:
+        B *= -1
+        A *= -1
+        C *= -1
+
+    # Clean output
     line = "line: "+str(B)+"x+"+str(A)+"y="+str(C)
     line = line.replace("+-","-")
-    line = line.replace("line: 1x","line: x")
-    line = line.replace("+1y","+y")
-    line = line.replace("-1y", "-y")
+    line = line.replace("+1y","+y").replace(" 1x"," x")
+    line = line.replace("-1y","-y")
     print(line)
 
     # Quit?
-    q = input("Quit? y/n: ")
+    q = input("Quit? y/n ")
     if q == "y":
         break
 
-# Quit
+#Quit
 print("Goodbye")
 quit()
